1f3bcaeb22932b987d00390d4a0e1e7a6047fdaa
albamboga <134630473+albamboga@users.noreply.github.com>
2023-07-01 15:32:02 +0200

    Update 4_🧮_Predictor.py

--- pages/4_🧮_Predictor.py
+++ pages/4_🧮_Predictor.py
@@ -4,6 +4,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from pycaret.regression import *
+import pycaret.internal.preprocess.transformers
 #--------------LIBRERÍAS--------------#
 
 #----------------------------CONFIGURACIÓN DE PÁGINAS----------------------------#
